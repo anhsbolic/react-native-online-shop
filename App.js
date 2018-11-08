a7c6a19f9c9acb04e8f23a4d13557e06f868f5c9
unknown <anhsbolic@gmail.com>
2018-11-08 13:17:13 +0700

    setup redux

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Navigator from './src/navigators/Navigator';
 
-export default class App extends React.Component {
+import { Provider } from 'react-redux';
+import configStore from './src/store/configStore';
+const store = configStore();
+
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <Navigator/>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
